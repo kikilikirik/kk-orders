a0cad211c626786e3a6e39da007e94f0b6f4a944
Kilian <kiliankager29@yahoo.com>
2025-05-05 19:59:53 +0200

    bla

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
+/*
+TODOS:
+1: rects machen
+2: rects in forschleife(doppel array bsp: let bla [[10, 10,[]]])
+3: farben ändern keine bestellung(0)= blau
+offene bestellung(1)= orange
+fertige bestellung(2)= grün
+4:
+*/ 
 import p5 from "p5";
+let tischimage = p5.Image;
+let 
 
 const p = new p5((sketch) => {
     sketch.setup = setup;
+    sketch.prelaod = prelaod;
 });
+function prelaod (){
+
+}
 
 function setup() {
     p.createCanvas(400, 400);
